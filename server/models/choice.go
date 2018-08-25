a14b4bb06daa2f6bab74ec684803aa5baa80ee1c
KamillKAPLAN <kamilkaplnn@gmail.com>
2018-08-25 18:53:13 +0300

    choice.go and option.go update

--- server/models/choice.go
+++ server/models/choice.go
@@ -1,19 +1,13 @@
 package models
 
-import (
-	"errors"
-	"net/http"
-	"strconv"
-
-	"github.com/labstack/echo"
-)
-
 type Choice struct {
-	ID       	   string        `json:"id"`
-	CategoryID     string        `json:"category_id"`
-	ProductID      string        `json:"product_id"`
-	OptionID       string        `json:"option_id"`
-	Name           string        `json:"name,omitempty"`
-	ListOrder      int64         `json:"list_order,omitempty"`
-	Price          float64       `json:"price,omitempty"`
+	ID         string  `json:"id"`
+	CategoryID string  `json:"category_id"`
+	ProductID  string  `json:"product_id"`
+	OptionID   string  `json:"option_id"`
+	Name       string  `json:"name,omitempty"`
+	ListOrder  int64   `json:"list_order,omitempty"`
+	Price      float64 `json:"price,omitempty"`
 }
+
+type Choices []Choice
--- server/models/init.go
+++ server/models/init.go
@@ -13,9 +13,13 @@ var CategorySlice Categories
 
 var ProductSlice Products
 
+var OptionSlice Options
+
+var ChoiceSlice Choices
+
 func Init() {
 	os.Chdir("server")
-
+	// categories.json
 	jsonFile, err := os.Open("categories.json")
 	if err != nil {
 		fmt.Println(err)
@@ -23,7 +27,7 @@ func Init() {
 	defer jsonFile.Close()
 	byteValue, _ := ioutil.ReadAll(jsonFile)
 	json.Unmarshal(byteValue, &CategorySlice)
-
+	// products.json
 	jsonFile2, err2 := os.Open("products.json")
 	if err2 != nil {
 		fmt.Println(err2)
@@ -31,4 +35,20 @@ func Init() {
 	defer jsonFile2.Close()
 	byteValue2, _ := ioutil.ReadAll(jsonFile2)
 	json.Unmarshal(byteValue2, &ProductSlice)
+	// option.json
+	jsonFile3, err3 := os.Open("option.json")
+	if err3 != nil {
+		fmt.Println(err3)
+	}
+	defer jsonFile3.Close()
+	byteValue3, _ := ioutil.ReadAll(jsonFile3)
+	json.Unmarshal(byteValue3, &OptionSlice)
+	// choice.json
+	jsonFile4, err4 := os.Open("choice.json")
+	if err4 != nil {
+		fmt.Println(err4)
+	}
+	defer jsonFile4.Close()
+	byteValue4, _ := ioutil.ReadAll(jsonFile4)
+	json.Unmarshal(byteValue4, &ChoiceSlice)
 }
